42aef08d724540483b6828a22658ced239cce512
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-03-18 21:23:30 -0500

    second update

--- PCA_IPCA.py
+++ PCA_IPCA.py
@@ -32,7 +32,33 @@ def run():
     # cada que usamos esta funcion va a partir los datos entre entrenamiento y pruebas, pero el porcentage
     # de uso de estos siempre es aleatorio , con randon state lo que hacemos es que fijamos esta aleatoriedad, para que siempre nos de 
     # la misma conclusion
+    print(x_train.shape) # ver la forma de los datos .shape  output es (numeros filas, numeros columnas)
+    print(y_train.shape)
 
+    # por defecto si no ponemos un n_componentes = min (n_muestas(filas), n featues(columnas))
+    pca = PCA (n_components = 3)
+    pca.fit(x_train)
+
+    #incremental pca no envia todos los datos a entrenar de una, crea pequeños bloques donde los manda a netrenar
+    #poco a poco
+    ipca = IncrementalPCA(n_components=3, batch_size=10)
+    ipca.fit(x_train)
+
+    plt.plot(range(len(pca.explained_variance_)), pca.explained_variance_ratio_) #eje y valor % que representa el dato
+    plt.show()
+
+    logistic = LogisticRegression(solver='lbfgs')
+
+    dt_train = pca.transform(x_train)
+    dt_test = pca.transform(x_test)
+    logistic.fit(dt_train, y_train)
+    print("SCORE PCA:",logistic.score(dt_test, y_test))
+
+    dt_train = ipca.transform(x_train)
+    dt_test = ipca.transform(x_test)
+    logistic.fit(dt_train, y_train)
+    print("SCORE IPCA:",logistic.score(dt_test, y_test)) # como vemos el rendimiento es casi el mismo  y con pca solo usamos 3 features
+    #y obtuvimos un resultado bueno ahorrando arto coste computacional
 
 
 if __name__ == '__main__':
