70deab24216dff71984d0a1d2a2608cb23f397e6
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-03-18 19:56:37 -0500

    First commit

---
+++ PCA_IPCA.py
@@ -0,0 +1,27 @@
+
+import pandas as pd # excel pa python
+import sklearn      # scikit-learn
+import matplotlib.pyplot as plt  #la de siempre bb
+
+#importamos los modulos de sklearn que nos serviran pa la decomposicion de los features
+
+from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
+
+#para hacer prueba de rendimiento de las implemetaciones, usamos un clasificador que importamos ahora
+
+from sklearn.linear_model import LogisticRegression
+
+#importamos dos utilidaddes para prepara los datos antes de mandarlos a procesamiento
+
+from sklearn.preprocessing import StandardScaler #para normalizar los datos de 0 a 1
+from sklearn.model_selection import train_test_split #partir los datos en entranamiento y testind
+
+
+def run():
+    jo
+
+
+
+if __name__ == '__main__':
+    run()
