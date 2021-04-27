d5c64042b7cb79df8980ed1528e0b0a6b55adbba
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-04-27 15:45:35 -0500

    adding prediction in simpleregression.oy

--- Regresion Lineal/simpleregression_sklearn.py
+++ Regresion Lineal/simpleregression_sklearn.py
@@ -15,7 +15,7 @@ def _countrys():
     return contry
 
 def model_salary_vs_countrys():
-    dataset = pd.read_csv('./salarios_dataset.csv') # load dataser
+    dataset = pd.read_csv('../datasets/salarios_dataset.csv') # load dataser
     feature = np.reshape(_countrys(), (30,1)) # converting list 1D to 2D
     target = dataset['Salario'].values           # split target variable
     x_train, x_test, y_train, y_test = train_test_split(feature, target, test_size=0.2, random_state=0) # split testing set and training set
@@ -26,7 +26,7 @@ def model_salary_vs_countrys():
 
 
 def _train():
-    dataset = pd.read_csv('./salarios_dataset.csv') # load dataset
+    dataset = pd.read_csv('../datasets/salarios_dataset.csv') # load dataset
     feature = dataset.iloc[:, :-1].values           # split feature variable
     target = dataset.iloc[:, 1].values              # split target variable
     x_train, x_test, y_train, y_test = train_test_split(feature, target, test_size=0.2, random_state=0) # split testing set and training set
@@ -43,7 +43,16 @@ def model_salary_vs_years():
     gaph(x_test, y_test, 'b', 'black', x_train, regressor)
     # print model score
     print(regressor.score(x_test, y_test)) # output : 0.786243  this is model accuracy 
+    making_predictions(regressor)
 
+def making_predictions(model):
+    # What species of iris would this be?
+    x_new=np.array([[2]])
+    # To make a prediction, call the predict method:
+    prediction = model.predict(x_new)
+    print(f"Prediction value: {prediction}")
+
+    
 
 def gaph(x, y, c1, c2, x_train, regressor):
     plt.scatter(x, y, color=c1)
@@ -56,8 +65,8 @@ def gaph(x, y, c1, c2, x_train, regressor):
 
 
 def run():
-    # model_salary_vs_years()
-    model_salary_vs_countrys()
+    model_salary_vs_years()
+    #model_salary_vs_countrys()
 
 
 if __name__ == '__main__':
